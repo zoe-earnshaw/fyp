1b062c2201937b009a2b0c5ae355038ec3bec792
zoe-earnshaw <53943274+zoe-earnshaw@users.noreply.github.com>
2020-04-01 16:23:45 +0100

    Update biggraph.py

--- biggraph.py
+++ biggraph.py
@@ -2,20 +2,20 @@ import ROOT
 import time
 import csv
 
-#outputfile = ROOT.TFile("big_graph.root", "recreate")
+outputfile = ROOT.TFile("big_graph.root", "recreate")
 
 h_significance = ROOT.TH2F("my graph", "my graph", 10, 1000, 2000, 10, 0, 1800)
 
 with open('best_significance.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        print(row)
-        print(" ")
-        print(row[2])
-        print(type(row[2]))
-#        h_significance.Fill(row[0], row[1], row[2])
+#        print(row)
+#        print(" ")
+#        print(row[2])
+#        print(type(row[2]))
+        h_significance.Fill(float(row[0]), float(row[1]), float(row[2]))
         
-#outputfile.Write()
-#outputfile.Close()
+outputfile.Write()
+outputfile.Close()
 
 
