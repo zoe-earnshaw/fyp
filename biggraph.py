24e1c7a6d1e76a94f4b39260f9afcc96d9cba447
zoe-earnshaw <53943274+zoe-earnshaw@users.noreply.github.com>
2020-04-01 16:21:30 +0100

    testing biggraph

--- biggraph.py
+++ biggraph.py
@@ -9,7 +9,11 @@ h_significance = ROOT.TH2F("my graph", "my graph", 10, 1000, 2000, 10, 0, 1800)
 with open('best_significance.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        h_significance.Fill(row[0], row[1], row[2])
+        print(row)
+        print(" ")
+        print(row[0])
+        print(type(row[0])
+#        h_significance.Fill(row[0], row[1], row[2])
         
 outputfile.Write()
 outputfile.Close()
