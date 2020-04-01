42dda4e8b22dd84a6476203b02c06621b9e95639
zoe-earnshaw <53943274+zoe-earnshaw@users.noreply.github.com>
2020-04-01 14:16:12 +0100

    Update significance.py

--- significance.py
+++ significance.py
@@ -105,7 +105,8 @@ for h_att in h_att_list:
             significance = ROOT.RooStats.NumberCountingUtils.BinomialExpZ(signal_events, totalbg_events, 0.15)
 
             #record best significance
-            if (h_att == nbjets) and (cut == cuts[0]):
+            if (h_att == nbjets) and (cut == cuts[1]):
+                print('the best significance is: ' + significance)
                 best_significance = significance
             
 #            print("Cut at ", cut, h_att.hist_name)
