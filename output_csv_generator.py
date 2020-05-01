24f3fb3ca83dc35c14a9af49a84dc83601ddefa8
zoe-earnshaw <53943274+zoe-earnshaw@users.noreply.github.com>
2020-05-01 11:25:44 +0100

    first try

--- output_csv_generator.py
+++ output_csv_generator.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python 
 import argparse
 import ROOT, sys
+import csv
 
 #take stop and lsp mass for chosen signal point from input
 parser = argparse.ArgumentParser()
@@ -14,64 +15,34 @@ process_names = ['signal*directTT*' + args.stop_mass + '_' + args.lsp_mass + '.*
 #for each process:
 for name in process_names:
     print(name)
+        
+    if 'signal' in name:
+        is_signal = 1
+    else:
+        is_signal = 0
     
+    with open("csvfile/" + name + "_data_" + args.stop_mass + "_" + args.lsp_mass + ".csv", 'wb') as csvfile:
+        cutwriter = csv.writer(csvfile)
+        cutwriter.writerow(['met','njets','nbjets','nnonbjets','top1mass','sumet','antikt8m0','antikt8m1','antikt12m0','antikt12m1','drbb'])
+
     #creating Tchain
     testchain = ROOT.TChain("StopZeroLeptonUpgrade__ntuple")
     #adding root files for given process
     testchain.Add("/lustre/scratch/epp/atlas/iv41/OUTPUT/UpgradeAnalysisOutput/LargeDM/" + name + "*_NTUP.root")
    
     #creating output file
-    file_name = name
-    if "signal" in name:
-        file_name = "signal" + "_" + args.stop_mass + "_" + args.lsp_mass
-    outputfile = ROOT.TFile(file_name + ".output.root", "recreate")
 
     print(testchain.GetEntries())
-
-    #creating histograms for variables
-    h_met = ROOT.TH1F("h_met"," ",100,0,2000)
-    h_nbjets = ROOT.TH1F("h_nbjets"," ",10,0,10)
-    h_njets = ROOT.TH1F("h_njets"," ",20,0,20)
-    h_nnonbjets = ROOT.TH1F("h_nnonbjets"," ",20,0,20)
-    h_top1mass = ROOT.TH1F("h_top1mass"," ",100,-2000,2000)
-    h_sumet = ROOT.TH1F("h_sumet"," ",100,0,5000)
-    h_antikt8m0 = ROOT.TH1F("h_antikt8m0"," ",100,0,1000)
-    h_antikt8m1 = ROOT.TH1F("h_antikt8m1"," ",100,0,1000)
-    h_antikt12m0 = ROOT.TH1F("h_antikt12m0"," ",100,0,1000)
-    h_antikt12m1 = ROOT.TH1F("h_antikt12m1"," ",100,0,1000)
-    h_drbb = ROOT.TH1F("h_drbb"," ",100,0,5)
-
-    #should have 2 signals at different delta m
 
     #applying cuts
     counter = 0
     for entry in testchain:
-        if (entry.NBJets < 2) or \
-                (entry.AntiKt12M_1 < 100) or \
-                (entry.AntiKt12M_1 > 250) or \
-                (entry.SumEt < 1500) or \
-                (entry.AntiKt8M_0 < 150) or \
-                (entry.AntiKt8M_1 < 150) or \
-                (entry.Met < 800):
+        if (entry.NBJets < 2):
             continue
-        #filling variable hists & weighting, if entry passes the cuts
-        h_met.Fill(entry.Met, entry.GlobalWeight)
-        h_nbjets.Fill(entry.NBJets, entry.GlobalWeight)
-        h_njets.Fill(entry.NJets, entry.GlobalWeight)
-        h_nnonbjets.Fill(entry.NNonBJets, entry.GlobalWeight)
-        h_top1mass.Fill(entry.top1M, entry.GlobalWeight)
-        h_sumet.Fill(entry.SumEt, entry.GlobalWeight)
-        h_antikt8m0.Fill(entry.AntiKt8M_0, entry.GlobalWeight)
-        h_antikt8m1.Fill(entry.AntiKt8M_1, entry.GlobalWeight)
-        h_antikt12m0.Fill(entry.AntiKt12M_0, entry.GlobalWeight)
-        h_antikt12m1.Fill(entry.AntiKt12M_1, entry.GlobalWeight)
-        h_drbb.Fill(entry.DRBB, entry.GlobalWeight)
+        #writing to csv, if entry passes the cuts
+        
+        cutwriter.writerow([entry.Met, entry.NBJets, entry.NJets, entry.NNonBJets, entry.top1M, entry.SumEt, entry.AntiKt8M_0, entry.AntiKt8M_1, entry.AntiKt12M_0, entry.AntiKt12M_1, entry.DRBB, is_signal])
 
         counter += 1
         if counter%1000 == 0:
             print("Counter = ", counter)
-
-    outputfile.Write()
-    outputfile.Close()
-
-#TDirectory to structure
