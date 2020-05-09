2e762092f7dada6adbfe48ef5b1e54ad5c9c4a75
zoe-earnshaw <53943274+zoe-earnshaw@users.noreply.github.com>
2020-05-09 17:49:06 +0100

    Update n-1_v1.py

--- n-1_v1.py
+++ n-1_v1.py
@@ -147,7 +147,7 @@ for h_att in h_att_list:
         i += h_att.step
 
 #    cuts = np.linspace(h_att.start, h_att.stop, h_att.step)
-    h_cuts = ROOT.TH1F('cuts', 'cuts;'+h_att.x_axis_title+';significance', h_att.step, h_att.start, h_att.stop)
+    h_cuts = ROOT.TH1F('cuts', 'cuts;'+h_att.x_axis_title+';significance', (h_att.stop-h_att.start)/h_att.step, h_att.start, h_att.stop)
 
     with open(h_att.hist_name+"_cuts.csv", 'wb') as csvfile:
         cutwriter = csv.writer(csvfile)
